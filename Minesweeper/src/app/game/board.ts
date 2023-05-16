bc42cca5c8d9aa188fd9669193131ba3d3803595
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-05-16 17:02:33 -0600

    create board

--- Minesweeper/src/app/game/board.ts
+++ Minesweeper/src/app/game/board.ts
@@ -1,5 +1,24 @@
+import { Cell } from "./cell";
+
 class board {
+    cells: Cell[][]=[];
     constructor(size:number, mines:number) {
-        
+        for(let y = 0; y<size;y++){
+            this.cells[y] = [];
+            for(let x = 0; x<size;x++){
+                this.cells[y][x] = new Cell();
+            }
+        }
+
+        //asign mines
+        for(let i = 0; i<mines;i++){
+            this.getRandomCell().mine =true;
+        }
+    }
+
+    getRandomCell(): Cell{
+        const y = Math.floor(Math.random() * this.cells.length);
+        const x =  Math.floor(Math.random() * this.cells[y].length);
+        return this.cells[y][x];
     }
 }
