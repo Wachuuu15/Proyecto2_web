c57f023708f14ef559219df7b2fa574c6d02ff81
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-05-16 20:10:30 -0600

    end

--- Minesweeper/src/app/game/board.ts
+++ Minesweeper/src/app/game/board.ts
@@ -1,12 +1,28 @@
 import { Cell } from "./cell";
 
-class board {
+const PEERS=[
+    [-1,1],
+    [-1,0],
+    [-1,1],
+    [0,-1],
+    [0,1],
+    [1,-1],
+    [1,0],
+    [1,1],
+];
+
+export class Board {
     cells: Cell[][]=[];
+    
+    private remainingCells = 0;
+    private mineCount= 0;
+
+
     constructor(size:number, mines:number) {
         for(let y = 0; y<size;y++){
             this.cells[y] = [];
             for(let x = 0; x<size;x++){
-                this.cells[y][x] = new Cell();
+                this.cells[y][x] = new Cell(y,x);
             }
         }
 
@@ -14,6 +30,26 @@ class board {
         for(let i = 0; i<mines;i++){
             this.getRandomCell().mine =true;
         }
+
+        //count mines
+       
+
+        for(let y = 0; y<size;y++){
+            for(let x = 0; x<size;x++){
+                let adjacentMines = 0;
+                for (const peer of PEERS) {
+                    if (this.cells[y+peer[0]] && this.cells[y+peer[0]][x+peer[1]] && this.cells[y+peer[0]][x+peer[1]].mine) {
+                        adjacentMines++;
+                    }
+                }
+                this.cells[y][x].proximityMines = adjacentMines;
+                if (this.cells[y][x].mine) {
+                    this.mineCount++;
+                }
+            }
+        }
+        this.remainingCells = size * size - this.mineCount;
+        
     }
 
     getRandomCell(): Cell{
@@ -21,4 +57,45 @@ class board {
         const x =  Math.floor(Math.random() * this.cells[y].length);
         return this.cells[y][x];
     }
+
+    checkCell(cell: Cell): 'gameover' | 'win' | null {
+        if (cell.status !== 'open') {
+          return null;
+        } else if (cell.mine) {
+          this.revealAll();
+          return 'gameover';
+        } else {
+          cell.status = 'clear';
+    
+          // Empty cell, let's clear the whole block.
+          if (cell.proximityMines === 0) {
+            for (const peer of PEERS) {
+              if (
+                this.cells[cell.row + peer[0]] &&
+                this.cells[cell.row + peer[0]][cell.column + peer[1]]
+              ) {
+                this.checkCell(this.cells[cell.row + peer[0]][cell.column + peer[1]]);
+              }
+            }
+          }
+    
+          if (this.remainingCells-- <= 1) {
+            return 'win';
+          }
+          return null;
+        }
+    }
+    
+
+    revealAll() {
+        for (const row of this.cells) {
+          for (const cell of row) {
+            if (cell.status === 'open') {
+              cell.status = 'clear';
+            }
+          }
+        }
+    }
+    
+    
 }
