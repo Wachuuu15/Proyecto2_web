8d1332a06fe508c60c5dc2b749ecc76362f7b6f9
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2023-05-19 19:57:58 -0600

    add color

--- Minesweeper/src/app/app.component.ts
+++ Minesweeper/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Board } from './game/board';
 import { Cell } from './game/cell';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
