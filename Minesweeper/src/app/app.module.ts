12b77f47c72fac3598aca362abcf9a9d72c03f94
FernandezDL <dianaferbandez31@gmail.com>
2023-05-25 16:49:40 -0600

    ultima pagina

--- Minesweeper/src/app/app.module.ts
+++ Minesweeper/src/app/app.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/board', pathMatch: 'full' },
   { path: 'board', component: BoardComponent },
   { path: 'authors-girls', component: AuthorsGirlsComponent },
+  { path: 'photo', component: PhotoComponent },
 ];
 
 @NgModule({
